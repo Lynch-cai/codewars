f27b51bf3a97ec299b5dee5940fa3afbf78aab60
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-11-30 20:51:08 +0100

    :tada: Valid Parentheses update

--- 011-valid_parentheses/code.js
+++ 011-valid_parentheses/code.js
@@ -2,29 +2,28 @@
 function validParentheses(parens) {
     let result = true
     let valid = 0
-    
     for (let i = 0; i < parens.length; i++){
-      if (valid >= 0) {
-        if (parens[i] == "(") {
-          valid++
+        if (valid >= 0) {
+            if (parens[i] == "(") {
+            valid++
+            }
+            else if (parens[i] == ")") {
+                valid--
+            }
         }
-        else if (parens[i] == ")") {
-          valid--
+        // if valid <= -1
+        else {
+            result = false
         }
-      }
-      // if valid <= -1
-      else {
-        result = false
-      }
     }
     if (valid != 0) {
-      result = false
+        result = false
     }
     return result 
-  }
+}
 
 
-  
+
 // -- Instruction
 
 // Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
