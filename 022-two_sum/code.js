6b6a41d4cb8eca3387fa348e659b6bdf25c0f6da
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-04 13:11:49 +0100

    :tada: Two Sum

---
+++ 022-two_sum/code.js
@@ -0,0 +1,42 @@
+// Two Sum
+function twoSum(numbers, target) {
+    console.log(numbers, target)
+    let notFounded = true
+    let i = 0
+    let res = 0
+    while (notFounded) {
+        res = numbers.findIndex(e => e == target - numbers[i]);
+        if (res !== -1 && res !== i){
+            return [i, res]
+        }
+        i++
+    }
+}
+
+
+
+// -- Instruction
+// Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+
+// For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+
+// The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+
+// Based on: http://oj.leetcode.com/problems/two-sum/
+
+// twoSum [1, 2, 3] 4 === (0, 2)
+
+
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//         function numericalCompare(a, b) {
+//             return a - b;
+//         }
+//         Test.assertSimilar(twoSum([1,2,3], 4).sort(numericalCompare), [0,2]);
+//         Test.assertSimilar(twoSum([1234,5678,9012], 14690).sort(numericalCompare), [1,2]);
+//         Test.assertSimilar(twoSum([2,2,3], 4).sort(numericalCompare), [0,1]);
+//     });
+// });
+  
