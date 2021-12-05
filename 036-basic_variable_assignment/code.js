8b43e632e0f7ecaa4f376c135e0de0ae48394d4b
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-05 20:47:49 +0100

    :tada: Basic variable assignment

---
+++ 036-basic_variable_assignment/code.js
@@ -0,0 +1,20 @@
+// Basic variable assignment
+let a = "code";
+let b = "wa.rs";
+let name = a + b;
+
+
+
+// -- Instruction
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+
+
+// -- Sample Tests:
+// const { assert } = require("chai")
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(name, "codewa.rs");
+//     })
+// })
