1410a3625b286ce723de4386b21a9cefbbe54f5a
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-05 22:55:25 +0100

    :tada: Name Shuffler

---
+++ 045-name_shuffler/code.js
@@ -0,0 +1,21 @@
+// Name Shuffler
+function nameShuffler(str){
+    return str.split(' ')[1] + ' ' + str.split(' ')[0]
+}
+
+
+
+// -- Instruction
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(nameShuffler('john McClane'),'McClane john');
+//     });
+//   });
+  
