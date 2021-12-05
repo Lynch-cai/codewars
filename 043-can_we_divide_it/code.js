9cf183a1278bfb5ae05bfa3a909ad5896fb454e4
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-05 22:44:34 +0100

    :tada: Can we divide it?

---
+++ 043-can_we_divide_it/code.js
@@ -0,0 +1,39 @@
+// Can we divide it?
+function isDivideBy(number, a, b) {
+    return number%a || number%b ? false : true
+}
+
+
+
+// -- Instruction
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+// A few cases:
+
+
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+
+// (45, 1, 6)    ->  false
+// (45, 5, 15)   ->  true
+
+// (4, 1, 4)     ->  true
+// (15, -5, 3)   ->  true
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(isDivideBy(-12, 2, -6), true);
+//     assert.strictEqual(isDivideBy(-12, 2, -5), false);
+//     assert.strictEqual(isDivideBy(45, 1, 6), false);
+//     assert.strictEqual(isDivideBy(45, 5, 15), true);
+//     assert.strictEqual(isDivideBy(4, 1, 4), true);
+//     assert.strictEqual(isDivideBy(15, -5, 3), true);
+//   });
+// });
