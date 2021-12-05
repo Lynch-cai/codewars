bbad1e096fbf05b7e04202c6c2655104d735b23c
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-05 22:35:16 +0100

    :tada: Convert boolean values to strings 'Yes' or 'No'

---
+++ 041-convert_boolean_value_to_strings_yes_or_no/code.js
@@ -0,0 +1,21 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord( bool ){
+    return bool ? 'Yes' : 'No'
+}
+
+
+
+// -- Instruction
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+
+// -- Sample Tests:
+// const { assert } = require("chai")
+
+// describe("Basic tests", () => {
+//   it("Testing for basic tests", () => {
+//     assert.strictEqual(boolToWord(true), 'Yes')
+//     assert.strictEqual(boolToWord(false), 'No')
+//     });
+//   });
