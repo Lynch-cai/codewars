dd2007bc431ac13eda910234242e6dc8a54dca87
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-11 22:10:21 +0100

    :tada: Will you survive the zombie onslaught?

---
+++ 084-will_you_survive_the_zombie_onslaught/code.js
@@ -0,0 +1,60 @@
+// Will you survive the zombie onslaught?
+function zombie_shootout(zombies, range, ammo) {
+    let zombiesStart = Number(zombies)
+    while (true) {
+        if (range > 0){
+            if(ammo >= 1){
+                ammo -= 1
+                zombies -= 1
+            }
+            else{
+                ammo = -1
+            }
+            range -= 0.5
+        }
+    
+        if(range <= 0 && zombies >= 1){
+            if (ammo >= 0 && ammo >= 0){
+                return `You shot ${zombiesStart - zombies} zombies before being eaten: overwhelmed.`
+            }
+            else {
+                return `You shot ${zombiesStart - zombies} zombies before being eaten: ran out of ammo.`
+            }
+        }
+        else if(zombies == 0){
+            return `You shot all ${zombiesStart} zombies.`
+        }
+    }
+}
+
+
+
+// -- Instruction
+// I'm afraid you're in a rather unfortunate situation. You've injured your leg, and are unable to walk, and a number of zombies are shuffling towards you, intent on eating your brains. Luckily, you're a crack shot, and have your trusty rifle to hand.
+
+// The zombies start at range metres, and move at 0.5 metres per second. Each second, you first shoot one zombie, and then the remaining zombies shamble forwards another 0.5 metres.
+
+// If any zombies manage to get to 0 metres, you get eaten. If you run out of ammo before shooting all the zombies, you'll also get eaten. To keep things simple, we can ignore any time spent reloading.
+
+// Write a function that accepts the total number of zombies, a range in metres, and the number of bullets you have.
+
+// If you shoot all the zombies, return "You shot all X zombies." If you get eaten before killing all the zombies, and before running out of ammo, return "You shot X zombies before being eaten: overwhelmed." If you run out of ammo before shooting all the zombies, return "You shot X zombies before being eaten: ran out of ammo."
+
+// (If you run out of ammo at the same time as the remaining zombies reach you, return "You shot X zombies before being eaten: overwhelmed.".)
+
+// Good luck! (I think you're going to need it.)
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold = 0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(zombie_shootout(3, 10, 10), "You shot all 3 zombies.");
+//     assert.strictEqual(zombie_shootout(100, 8, 200), "You shot 16 zombies before being eaten: overwhelmed.");
+//     assert.strictEqual(zombie_shootout(50, 10, 8), "You shot 8 zombies before being eaten: ran out of ammo.");
+//   });
+// });
