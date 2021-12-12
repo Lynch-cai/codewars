bc38d8bbaf377d2d4e596734e7ce3a54926e12f3
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-12 12:24:52 +0100

    :tada: Perimeter sequence

---
+++ 086-perimeter_sequence/code.js
@@ -0,0 +1,25 @@
+// Perimeter sequence
+function perimeterSequence(a,n) {
+    return a*n*4
+}
+
+
+
+// -- Instruction
+// Perimeter sequence
+// The first three stages of a sequence are shown.
+
+// blocks
+
+// The blocksize is a by a and a ≥ 1.
+
+// What is the perimeter of the nth shape in the sequence (n ≥ 1) ?
+
+
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(perimeterSequence(1,3), 12)
+//     });
+// });
