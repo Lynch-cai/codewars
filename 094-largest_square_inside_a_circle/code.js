2916051c67c38fefa0e0e5cd78babc2c52af6747
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-13 21:51:27 +0100

    :tada: Largest Square Inside A Circle

---
+++ 094-largest_square_inside_a_circle/code.js
@@ -0,0 +1,24 @@
+// Largest Square Inside A Circle
+function areaLargestSquare(r) {
+    return r**2*2
+}
+
+
+
+// -- Instruction
+// Determine the area of the largest square that can fit inside a circle with radius r.
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Testing --- Area largest square --- ", () => {
+//   it("Basic tests", () => {
+//     assert.strictEqual(areaLargestSquare(5), 50);
+//     assert.strictEqual(areaLargestSquare(7), 98);
+//     assert.strictEqual(areaLargestSquare(15), 450);
+//   });
+// });
