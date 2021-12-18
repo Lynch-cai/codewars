f38827fce1b22482d247bba5f6461b502d34f6cb
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-18 12:45:36 +0100

    :tada: JavaScript Array Filter

---
+++ 117-javascript_array_filter/code.js
@@ -0,0 +1,28 @@
+// JavaScript Array Filter
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(n => !(n%2))
+}
+
+
+
+// -- Instruction
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+
+
+// -- Sample Tests:
+// const assert = require("chai").assert;
+
+// describe("Fixed tests", () => {
+//     it('should return an empty array if there are no even numbers', function(){
+//       assert.deepEqual(getEvenNumbers([1,2,3,6,8,10]), [2,6,8,10])
+//       assert.deepEqual(getEvenNumbers([1,2]), [2])
+//       assert.deepEqual(getEvenNumbers([12,14,15]), [12,14])
+//       assert.deepEqual(getEvenNumbers([13,15]), [])
+//       assert.deepEqual(getEvenNumbers([1,3,9]), [])
+//     });
+// })
