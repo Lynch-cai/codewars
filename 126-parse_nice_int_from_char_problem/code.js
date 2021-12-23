f93cccc695a65865e5ecceefc69c968303222489
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-23 23:15:26 +0100

    :tada: Parse nice int from char problem

---
+++ 126-parse_nice_int_from_char_problem/code.js
@@ -0,0 +1,26 @@
+// Parse nice int from char problem
+function getAge(inputString){
+    return Number(inputString[0])
+}
+
+
+
+// -- Instruction
+// Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+
+// For correct answer program should return int from 0 to 9.
+
+// Assume test input string always valid and may look like "1 year old" or "5 years old", etc.. The first char is number only.
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(getAge("4 years old"), 4);
+//   })
+// })
