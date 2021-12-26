bdde12da474ade8ce9f2b0282e949197dc5ba1de
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-26 21:48:43 +0100

    :tada: Switch it Up!

---
+++ 128-Switch_it_up/code.js
@@ -0,0 +1,31 @@
+// Switch it Up!
+function switchItUp(number){
+    const numbers = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return numbers[number]
+}
+
+
+
+// -- Instruction
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic Tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(switchItUp(1),"One");
+//     assert.strictEqual(switchItUp(3),"Three");
+//     assert.strictEqual(switchItUp(5),"Five");
+//   });
+// });
