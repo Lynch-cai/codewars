4697d9daf9f237ccbf82a2cc3a3af3ea58e27b03
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2022-01-10 22:49:45 +0100

    :tada: Keypad horror

---
+++ 135-keypad_horror/code.js
@@ -0,0 +1,45 @@
+// Keypad horror
+function computerToPhone(numbers){
+    let res = ''
+    const phoneKeyPad = [0,7,8,9,4,5,6,1,2,3]
+    for (let i = 0; i < numbers.length; i++){
+        res += phoneKeyPad[numbers[i]]
+    }
+    return res
+}
+
+
+
+// -- Instruction
+// Having two standards for a keypad layout is inconvenient!
+// Computer keypad's layout:
+// 7 8 9  \n
+// 4 5 6  \n
+// 1 2 3  \n
+//   0 \n
+
+// Cell phone keypad's layout:
+// 1 2 3\n 
+// 4 5 6\n  
+// 7 8 9\n  
+//   0\n
+
+// Solve the horror of unstandardized keypads by providing a function that converts computer input to a number as if it was typed on a phone.
+
+// Example:
+// "789" -> "123"
+
+// Notes:
+// You get a string with numbers only
+
+
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(computerToPhone("0789456123"), "0123456789")
+//   Test.assertEquals(computerToPhone("000"), "000")
+//   Test.assertEquals(computerToPhone("94561"), "34567")
+//   Test.assertEquals(computerToPhone(""), "")
+//     });
+// });
