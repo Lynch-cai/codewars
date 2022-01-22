f06762180107920fd58105550c1213912232a482
Lynch Cai <binkacai@gmail.com>
2022-01-22 19:57:37 +0100

    :tada: You Can't Code Under Pressure #1

---
+++ 145-you_cant_code_under_pressure/code.js
@@ -0,0 +1,14 @@
+// You Can't Code Under Pressure #1
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// -- Instruction
+// Code as fast as you can! You need to double the integer and return it.
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(doubleInteger(2), 4);
+//     });
+// });
