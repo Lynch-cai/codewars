521b8cf6671c74fb2be13f1896336aec9bfb39cd
Lynch Cai <binkacai@gmail.com>
2022-01-23 13:53:14 +0100

    :tada: Opposite number

---
+++ 146-opposite_number/code.js
@@ -0,0 +1,18 @@
+// Opposite number
+const opposite = (n) => n * -1;
+
+// -- Instruction
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(opposite(1), -1,)
+//     });
+// });
