73890c223907a5664dc3e90cffd2b096c132e18f
Lynch Cai <binkaicai@gmail.com>
2022-01-29 21:52:23 +0100

    :tada: Returning Strings

---
+++ 149-returning_strings/code.js
@@ -0,0 +1,23 @@
+// Returning Strings
+const greet = (name) => `Hello, ${name} how are you doing today?`
+
+
+
+// -- Instruction
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+//     assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+//   })
+// })
