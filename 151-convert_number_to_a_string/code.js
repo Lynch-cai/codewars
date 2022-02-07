965ce57e4912c69a259b753c33f15f77f49750c6
Lynch Cai <binkaicai@gmail.com>
2022-02-07 20:32:12 +0100

    :tada: Convert a Number to a String!

---
+++ 151-convert_number_to_a_string/code.js
@@ -0,0 +1,22 @@
+// Convert a Number to a String!
+const numberToString = n => n.toString()
+
+
+
+// -- Instruction
+// We need a function that can transform a number into a string.
+
+// What ways of achieving this do you know?
+
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+
+
+// -- Sample Tests:
+// describe("Tests", () => {
+//     it("test", () => {
+//   Test.assertEquals(numberToString(67), '67');
+//     });
+// });
