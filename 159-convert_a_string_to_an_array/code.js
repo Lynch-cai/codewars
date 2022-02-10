ed4b1eddb9c421d3e1907daa17d009e0f8270115
Lynch Cai <binkaicai@gmail.com>
2022-02-10 19:34:27 +0100

    :tada: Convert a string to an array

---
+++ 159-convert_a_string_to_an_array/code.js
@@ -0,0 +1,23 @@
+// Convert a string to an array
+const stringToArray = str => str.split(' ')
+
+
+
+// -- Instruction
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+//     assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
+//   });
+// });
