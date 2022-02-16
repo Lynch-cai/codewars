07b42932c87a0ccbb219a6b031082a648d7c1bf3
Lynch Cai <binkaicai@gmail.com>
2022-02-16 16:54:36 +0100

    :tada: Third Angle of a Triangle

---
+++ 177-third_angle_of_a_triangle/code.js
@@ -0,0 +1,29 @@
+// Third Angle of a Triangle
+const otherAngle = (a, b) => 180-a-b
+
+
+
+// -- Instruction
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(otherAngle(30, 60), 90);
+//     assert.strictEqual(otherAngle(60, 60), 60);
+//     assert.strictEqual(otherAngle(43, 78), 59);
+//     assert.strictEqual(otherAngle(10, 20), 150);
+//   });
+// });
