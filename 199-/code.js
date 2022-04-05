079a39c828ebd1414b15c79564130a80a956beea
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2022-04-05 23:23:45 +0200

    :tada: Count Odd Numbers below n

--- 199-/code.js
+++ 199-/code.js
@@ -1,11 +1,25 @@
-// 
-
+// Count Odd Numbers below n
+const oddCount = n => Math.floor(n/2)
 
 
 
 // -- Instruction
+// Given a number n, return the number of positive odd numbers below n, EASY!
 
+// Examples (Input -> Output)
+// * 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// * 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
 
 
 
 // -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//    it("Testing for fixed tests", () => {
+//     assert.strictEqual(oddCount(15), 7, "Oops! Wrong.");
+//     assert.strictEqual(oddCount(15023), 7511, "Oops! Wrong.");
+//   });
+// });
