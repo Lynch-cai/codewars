996609fab688e0c8e10a7bb88ca9525b9205b270
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2022-04-05 23:22:27 +0200

    :tada: Remove exclamation marks

---
+++ 198-remove_exclamation_marks/code.js
@@ -0,0 +1,20 @@
+// Remove exclamation marks
+const removeExclamationMarks = s => (s.replace(/!/g, ''))
+
+
+
+// -- Instruction
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+
+// -- Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(removeExclamationMarks("Hello World!"), "Hello World");
+//   });
+// });
