b128473af8a9ecf485a5b18dd90a59d59e7a237e
Lynch Cai <binkaicai@gmail.com>
2022-06-04 21:56:48 +0200

    :tada: Persistent Bugger

---
+++ 222-persistent-burger/code.ts
@@ -0,0 +1,42 @@
+// Persistent Bugger
+export function persistence(number: number): number {
+  let num: number = number
+  let res: number = 0
+  
+  const splitAndMultiplicate = (n: number): number => {
+    return n.toString().split('').map(Number).reduce((pv, cv) => pv * cv)
+  }
+  
+  while (num.toString().split('').length > 1) {
+    num = splitAndMultiplicate(num)
+    res++
+  }
+  
+  return res
+}
+
+
+
+// -- Instruction
+// Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+// For example (Input --> Output):
+
+// 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+// 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+// 4 --> 0 (because 4 is already a one-digit number)
+
+
+
+// -- Sample Tests:
+// import { assert } from "chai";
+// import { persistence } from "./solution";
+
+// describe("Persistent Bugger.", () => {
+//   it("Fixed tests", () => {
+//     assert.strictEqual(persistence(39),3);
+//     assert.strictEqual(persistence(4),0);
+//     assert.strictEqual(persistence(25),2);
+//     assert.strictEqual(persistence(999),4);
+//   });
+// });
