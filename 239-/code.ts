2299f142eb1ded726da60ab9b618626baafcd3fc
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2022-07-26 16:53:18 +0200

    :tada: Multiplying two numbers

--- 239-/code.ts
+++
@@ -1,9 +0,0 @@
-// 
-
-
-
-// -- Instruction
-
-
-
-// -- Sample Tests:
---
+++ 239-multiplying-two-numbers/code.ts
@@ -0,0 +1,21 @@
+// Multiplying two numbers
+export function multiply (a:number,b:number):number {
+  return a*b
+}
+
+
+
+// -- Instruction
+// Implement a function which multiplies two numbers.
+
+
+
+// -- Sample Tests:
+// import solution = require('./solution');
+// import {assert} from "chai";
+
+// describe("solution", function() {
+//   it("test", function() {
+//     assert.equal(solution.multiply(2, 3), 6)
+//   });
+// });
